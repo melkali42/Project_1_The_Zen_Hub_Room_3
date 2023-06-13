18ad4a52101e79fda9315d72d8786084ebc5e5b1
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-06-12 20:27:02 -0700

    updates to favorites list functionality and button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 // Favorites button add to favorites list
+var addFav = document.getElementById("addToFavorites");
+
 function addFavorite() {
     var favoritesList = JSON.parse(localStorage.getItem("allFavorites"));
     if(favoritesList == null) favoritesList = [];
-    var cityName = document.getElementById(city).value;
+    var cityName = document.getElementById("city").value;
 
 localStorage.setItem(cityName,JSON.stringify(cityName));
 favoritesList.push(cityName);
 localStorage.setItem("allFavorites", JSON.stringify(favoritesList));
+console.log(favoritesList);
 }
+
+addFav.addEventListener("click", function(addFavorite){
+    console.log("Done!");
+});
