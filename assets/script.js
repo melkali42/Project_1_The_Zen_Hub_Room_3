72fca4319fdcf20b3c98c66c29fef6d54c75f2b5
Melissa <melissakalish2@gmail.com>
2023-06-13 22:52:39 -0500

    work progress

--- assets/script.js
+++ assets/script.js
@@ -35,60 +35,12 @@ function getCurrentWeatherInfo(cityName) {
     .then((data) => {
       console.log(data);
     });
-}
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var cityName = searchInput.value;
-  getCurrentWeatherInfo(cityName);
-
-
-const apiKey="c3adaa7e48d20b2f65d6246f1225cb77"
-const searchBtn= document.querySelector(".searchBtn")
-const city= document.querySelector(".cityInput")
-const currentWeatherContainer=document.querySelector(".currentWeather")
-const forecastContainer=document.querySelector(".forecast")
-const historicalContainer=document.querySelector(".historicalSearch")
-const historyTitle=document.querySelector("#historyTitle")
-const sub=document.querySelector(".herosub")
-const todaysDate= dayjs().format("dddd, D MMMM YYYY")
-sub.textContent=todaysDate
-let cityArray=[]
-
-const getHistory=(searchHistory)=>{
-    const btn=document.createElement("button")
-    btn.setAttribute("class", "button")
-    btn.setAttribute("value", searchHistory)
-    btn.textContent=searchHistory
-    historicalContainer.append(btn)
-    if(searchHistory){
-        historyTitle.classList.remove("hide")
-    }
-}
-
-let searchHistory=JSON.parse(localStorage.getItem("history"))|| []
-console.log(searchHistory)
-for(let i=0; i<searchHistory.length; i++){
-    getHistory(searchHistory[i])
-}
-
-const getApiData=(userInput)=>{
-    console.log(userInput)
-    const getWeatherURL='https://api.openweathermap.org/data/2.5/weather?q={userInput}&appid={API key}&units=imperial'
-
-    fetch(getWeatherURL)
-    .then((response)=>{return response.json()})
-    .then((data)=>{
-        displayCurrentWeather(data)
-    })
-}
 
     const { Name } = data; 
     const { icon, description } = date.getWeatherURL[0];
     const { temp, humidity } = data.main
     const { speed } = data.wind
-    const { lat, long } = data.coord
-    console.log(Name, icon, temp, humidity, speed, lat, long)
+        console.log(Name, icon, temp, humidity, speed, lat, long)
 
     //To display Weather to the site
     document.querySelector(".cityInput").innerText = "Weather In" + Name;
@@ -97,4 +49,15 @@ const getApiData=(userInput)=>{
     document.queryselection(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".speed").innerText = "Wind Speed: " + speed + "MPH"
-}
+
+  }
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var cityName = searchInput.value;
+  getCurrentWeatherInfo(cityName);
+})
+
+
+
+
