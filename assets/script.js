89964c3bfe0ef5573b7af563006dcaa53354da39
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-06-13 19:51:44 -0700

    edits

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,26 @@
 // Favorites button add to favorites list
 var addFav = document.getElementById("addToFavorites");
 
-function addFavorite() {
-    var favoritesList = JSON.parse(localStorage.getItem("allFavorites"));
+function addFavorite() { 
+  var favoritesList = JSON.parse(localStorage.getItem("allFavorites"));
     if(favoritesList == null) favoritesList = [];
-    var cityName = document.getElementById("city").value;
+    var cityName = document.getElementById("search-input").value;
 
 localStorage.setItem(cityName,JSON.stringify(cityName));
 favoritesList.push(cityName);
 localStorage.setItem("allFavorites", JSON.stringify(favoritesList));
+
 console.log(favoritesList);
 }
 
-addFav.addEventListener("click", function (addFavorite){
-    console.log("Done!");
-  });
+addFav.addEventListener("click",addFavorite);
 
 var searchBar = document.querySelector(".search-bar");
 var searchButton = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
 var form = document.getElementById("form");
 var apiKey = "b2a3d794234a2e76abf165d172c1074d";
+
 // establish the API URL- save in a variable
 // make another function get forecast weather info
 
